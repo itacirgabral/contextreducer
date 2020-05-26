736a271d00c7213c6d8b4ad0405c86670272df35
Itacir Gabral <itacirgabral@gmail.com>
2020-05-25 20:42:00 -0400

    feat itens

--- src/pages/carrinho.js
+++ src/pages/carrinho.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const produtos = Array(3).fill({
+  name: 'nome',
+  price: 'preço',
+  img: 'imagem',
+}).map(
+  ({name, price, img}, i) => ({
+    name: `${name}-${i}`,
+    price: `${price}-${i}`,
+    img: `${img}-${i}`
+  })
+)
+
 const Carrinho = () => {
   return <>
     <p>Carrinho</p>
+    <ul>
+      {
+        produtos.map(({name, price, img}) => <li>
+          <p>
+            <span>{name} </span>
+            <span> {price} </span>
+            <span> {img} </span>
+            <button>devolver</button>
+          </p>
+        </li>)
+      }
+    </ul>
     <Link  to='/'>
       <span>voltar</span>
     </Link>
--- src/pages/gondola.js
+++ src/pages/gondola.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const produtos = Array(20).fill({
+  name: 'nome',
+  price: 'preço',
+  img: 'imagem',
+}).map(
+  ({name, price, img}, i) => ({
+    name: `${name}-${i}`,
+    price: `${price}-${i}`,
+    img: `${img}-${i}`
+  })
+)
+
 const Gondola = () => {
   return <>
     <h1>Gondola</h1>
+    <ul>
+      {
+        produtos.map(({name, price, img}) => <li>
+          <p>
+            <span>{name} </span>
+            <span> {price} </span>
+            <span> {img} </span>
+            <button>comprar</button>
+          </p>
+        </li>)
+      }
+    </ul>
     <Link  to='/carrinho'>
       <span>ver carrinho</span>
     </Link>
